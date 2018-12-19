e0f85973132594c607f799b0ea7cdfdc177f2c47
Bahtiar <32741688+BahtiarTuratbekov@users.noreply.github.com>
2018-12-19 18:27:39 +0600

    Modificated my files
    Modificated my 'AboutParents.jsx' component and 'AboutParents.css' files

--- src/components/AboutParents.jsx
+++ src/components/AboutParents.jsx
@@ -1,21 +1,73 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import '../css/AdoptiveParent.css';
+import '../css/AboutParents.css';
 import { Jumbotron, Grid, Row, Col, Image } from 'react-bootstrap';
 import Carousel from 'nuka-carousel';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
 export default class Home extends Component {
+    constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
       return (
         <div>
+        <Button color="danger">Danger!</Button>
           <div className="slider-list">
             <div className="slider-slide">
               <h1>InFamily</h1>
-                  <Carousel>
-                    <img height={300} alt="300x300" src="https://flytothesky.ru/wp-content/uploads/2017/06/98687-696x392.jpg" />
-                    <img height={300} alt="300x300" src="http://linesa.ru/uploads/posts/2016-03/1457265800_1.jpg" />
-                    <img height={300} alt="300x300" src="http://www.garmoniazhizni.com/wp-content/uploads/2015/11/vzaimoponimanie-v-seme.jpg" />
-                  </Carousel>
+                <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
             </div>
           </div>
           <Grid>
